4b1efcf6d2380ffd66e71f152aafde463a24d7da
msheffield <msheffield@adaptivebiotech.com>
2020-01-13 11:09:10 -0800

    added handlebars and initialized heroku with mongolab

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 
 var axios = require("axios");
@@ -13,10 +14,15 @@ var app = express();
 // Set up app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/scrapper", { useNewUrlPraser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+mongoose.connect(MONGODB_URI);
 
 
 // Start the server
